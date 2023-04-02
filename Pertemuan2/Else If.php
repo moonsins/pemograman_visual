e67098b51a9fd3cbb1d524a36ee35fba4edb9b7f
moonsins <129641932+moonsins@users.noreply.github.com>
2023-04-02 15:08:58 +0700

    Add files via upload

---
+++ Pertemuan2/Else If.php
@@ -0,0 +1,11 @@
+<?php
+   $x = 30;
+
+   if($x < 30){
+      echo "Benar"; 
+   } elseif ($x == 30 ){
+      echo "Bingo!"; 
+   } else {
+      echo "Salah";
+   }
+?>
---
+++ Pertemuan2/Else.php
@@ -0,0 +1,9 @@
+<?php
+   $x = 50;
+
+   if($x < 30){
+      echo "Benar!";
+   } else {
+      echo "Salah!";
+   }
+?>
